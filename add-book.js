398951d22a9f45dded9459466b4d65d9694a8dd0
web2ls <web2ls@yandex.ru>
2024-05-25 22:15:41 +0400

    added handler as func

--- add-book.js
+++ add-book.js
@@ -1,5 +1,25 @@
-function addBook(msg) {
-  console.log('message received');
+async function addBook(msg) {
+  const authorNamePrompt = await bot.sendMessage(msg.chat.id, 'Введите имя автора', {
+    reply_markup: {
+      force_reply: true,
+    }
+  });
+
+  bot.onReplyToMessage(msg.chat.id, authorNamePrompt.message_id, async (nameMsg) => {
+    const bookNamePrompt = await bot.sendMessage(msg.chat.id, 'Введите наименование книги', {
+      reply_markup: {
+        force_reply: true,
+      }
+    });
+
+    bot.onReplyToMessage(msg.chat.id, bookNamePrompt.message_id, async (bookNameMsg) => {
+      const bookNamePrompt = await bot.sendMessage(msg.chat.id, 'Введите наименование книги', {
+        reply_markup: {
+          force_reply: true,
+        }
+      });
+    })
+  })
 }
 
 module.exports = addBook;
