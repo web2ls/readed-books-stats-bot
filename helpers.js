4c0c40849412f9007e9e0ed5b3fe24e21e358f11
web2ls <web2ls@yandex.ru>
2024-06-23 17:57:23 +0400

    add debugger

--- helpers.js
+++ helpers.js
@@ -35,11 +35,15 @@ function getValidatedDate(input) {
     return null;
   }
 
+  console.log('date is valid', value);
+
   const dateList = value.split('.');
 
   if (dateList.length !== 3 || isNaN(dateList[0]) || isNaN(dateList[1]) || isNaN(dateList[2]) || dateList[2].length !== 4) {
     return null;
   }
+
+  console.log('return valid date', new Date(Number(dateList[2]), Number(dateList[1]) - 1, Number(dateList[0]) + 1, 0, 0, 0).toISOString());
 
   return new Date(Number(dateList[2]), Number(dateList[1]) - 1, Number(dateList[0]) + 1, 0, 0, 0).toISOString();
 }
