6c149dff944a0a3bf80337ee363030169d12dec0
web2ls <web2ls@yandex.ru>
2024-06-27 22:22:12 +0400

    fix cleaning reply listeners

--- tg-event-handlers/add-book-handler.js
+++ tg-event-handlers/add-book-handler.js
@@ -12,49 +12,61 @@ async function addBookHandler(bot, msg) {
       }
     });
 
-    bot.onReplyToMessage(msg.chat.id, authorNamePrompt.message_id, async (nameMsg) => {
+    const authorNamePromptListener = bot.onReplyToMessage(msg.chat.id, authorNamePrompt.message_id, async (nameMsg) => {
       const bookNamePrompt = await bot.sendMessage(msg.chat.id, 'Наименование книги', {
         reply_markup: {
           force_reply: true,
         }
       });
 
-      bot.onReplyToMessage(msg.chat.id, bookNamePrompt.message_id, async (bookNameMsg) => {
+      bot.removeReplyListener(authorNamePromptListener);
+
+      const bookNamePromptListener = bot.onReplyToMessage(msg.chat.id, bookNamePrompt.message_id, async (bookNameMsg) => {
         const startDatePrompt = await bot.sendMessage(msg.chat.id, 'Когда начали читать (в формате 5.10.2020)', {
           reply_markup: {
             force_reply: true,
           }
         });
 
-        bot.onReplyToMessage(msg.chat.id, startDatePrompt.message_id, async (startDateMsg) => {
+        bot.removeReplyListener(bookNamePromptListener);
+
+        const startDatePromptListener = bot.onReplyToMessage(msg.chat.id, startDatePrompt.message_id, async (startDateMsg) => {
           const endDatePrompt = await bot.sendMessage(msg.chat.id, 'Когда закончили читать (в формате 5.10.2020)', {
             reply_markup: {
               force_reply: true,
             }
           });
 
-          bot.onReplyToMessage(msg.chat.id, endDatePrompt.message_id, async (endDateMsg) => {
+          bot.removeReplyListener(startDatePromptListener);
+
+          const endDatePromptListener = bot.onReplyToMessage(msg.chat.id, endDatePrompt.message_id, async (endDateMsg) => {
             const pagesAmountPrompt = await bot.sendMessage(msg.chat.id, 'Количество страниц', {
               reply_markup: {
                 force_reply: true,
               }
             });
 
-            bot.onReplyToMessage(msg.chat.id, pagesAmountPrompt.message_id, async (pagesAmountMsg) => {
+            bot.removeReplyListener(endDatePromptListener);
+
+            const pagesAmountPromptListener = bot.onReplyToMessage(msg.chat.id, pagesAmountPrompt.message_id, async (pagesAmountMsg) => {
               const ratingPrompt = await bot.sendMessage(msg.chat.id, 'Введите рейтинг книги (по шкале от 1 до 5 включительно)', {
                 reply_markup: {
                   force_reply: true,
                 }
               });
 
-              bot.onReplyToMessage(msg.chat.id, ratingPrompt.message_id, async (ratingMsg) => {
+              bot.removeReplyListener(pagesAmountPromptListener);
+
+              const ratingPromptListener = bot.onReplyToMessage(msg.chat.id, ratingPrompt.message_id, async (ratingMsg) => {
                 const reviewPrompt = await bot.sendMessage(msg.chat.id, 'Напишите ваш отзыв на книгу', {
                   reply_markup: {
                     force_reply: true,
                   }
                 });
 
-                bot.onReplyToMessage(msg.chat.id, reviewPrompt.message_id, async (reviewMsg) => {
+                bot.removeReplyListener(ratingPromptListener);
+
+                const reviewPromptListener = bot.onReplyToMessage(msg.chat.id, reviewPrompt.message_id, async (reviewMsg) => {
                   const newBook = {
                     userId: msg.from.id,
                     author: nameMsg.text,
@@ -66,7 +78,8 @@ async function addBookHandler(bot, msg) {
                     review: reviewMsg.text,
                   }
 
-                  bot.clearReplyListeners();
+                  bot.removeReplyListener(reviewPromptListener);
+
                   await BookController.addBook(newBook);
                   await bot.sendMessage(
                     msg.chat.id,
@@ -80,7 +93,6 @@ async function addBookHandler(bot, msg) {
       })
     })
   } catch {
-    bot.clearReplyListeners();
     await errorHandler(msg.chat.id, bot, { message: 'Failed to add book' });
   }
 }
