423c25538e894403bd24675846516480536dd4cf
web2ls <web2ls@yandex.ru>
2024-07-25 23:04:21 +0400

    cleanup code

--- index.js
+++ index.js
@@ -6,16 +6,6 @@ const cors = require('cors');
 const pino = require('pino-http')();
 
 const db = require('./db');
-const COMMANDS = require('./commands');
-const addBookHandler = require('./tg-event-handlers/add-book-handler');
-const searchBookHandler = require('./tg-event-handlers/search-book-handler');
-const editBookHandler = require('./tg-event-handlers/edit-book-handler');
-const selectBookForEditHandler = require('./tg-event-handlers/select-book-for-edit-handler');
-const getAmountByCurrentMonthHandler = require('./tg-event-handlers/get-amount-by-current-month-handler');
-const getAmountByCurrentYearHandler = require('./tg-event-handlers/get-amount-by-current-year-handler');
-const deleteBookHandler = require('./tg-event-handlers/delete-book-handler');
-const getBooksListByCurrentMonthHandler = require('./tg-event-handlers/get-books-list-by-current-month-handler');
-const { closeMenu, openQuickStatsMenu } = require('./tg-event-handlers/menu-handler');
 
 const { addBook, editBook, deleteBook, getBook, searchBook, getQuickStats } = require('./api');
 
@@ -46,7 +36,6 @@ db.serialize(() => {
 const PORT = process.env.API_PORT;
 const TOKEN = process.env.BOT_TOKEN;
 const bot = new TelegramBot(TOKEN, {polling: true});
-bot.setMyCommands(COMMANDS);
 
 app.use(express.static('dist'));
 app.use(express.json());
@@ -63,26 +52,6 @@ bot.onText(/\/start/, async (msg) => {
     console.log('Error due processing /start command');
   }
 })
-
-bot.onText(/\/add/, addBookHandler.bind(this, bot));
-
-bot.onText(/\/find/, searchBookHandler.bind(this, bot));
-
-bot.onText(/\/quickstats/, openQuickStatsMenu.bind(this, bot));
-
-bot.onText(/^Количество за месяц$/, getAmountByCurrentMonthHandler.bind(this, bot));
-
-bot.onText(/^Количество за год$/, getAmountByCurrentYearHandler.bind(this, bot));
-
-bot.onText(/^Список книг за месяц$/, getBooksListByCurrentMonthHandler.bind(this, bot));
-
-bot.onText(/^(Автор|Наименование|Начали|Закончили|Страницы|Рейтинг|Обзор).*\[[0-9]*\]$/, editBookHandler.bind(this, bot));
-
-bot.onText(/^(?!Автор|Наименование|Начали|Закончили|Страницы|Рейтинг|Обзор).*\[[\0-9]*\]$/, selectBookForEditHandler.bind(this, bot));
-
-bot.onText(/^Удалить книгу .*/, deleteBookHandler.bind(this, bot));
-
-bot.onText(/^Закрыть меню$/, closeMenu.bind(this, bot));
 
 bot.on("polling_error", err => console.log(err.data.error.message));
 
